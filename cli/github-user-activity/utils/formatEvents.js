546c83ad4ed1e85f3b96c7a17961012183760ee4
lucky-blessed <blessednkwor7@gmail.com>
2026-03-09 01:20:53 -0600

    Added pushedEvent formatter

--- cli/github-user-activity/utils/formatEvents.js
+++ cli/github-user-activity/utils/formatEvents.js
@@ -1,5 +1,13 @@
 function formatEvents(events) {
     return events.map(event => {
+
+        if (event.type === "PushedEvent") {
+            const commitCount = event.payload.commits.length;
+            const repoName = event.repo.name;
+
+            return `Pushed ${commitCount} commit(s) to ${repoName}`;
+        }
+        
         return event.type;
     });
 }
