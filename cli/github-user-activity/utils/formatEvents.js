3ca86e8298e3045762ed3223bbee21265ff25d32
lucky-blessed <blessednkwor7@gmail.com>
2026-03-09 01:38:39 -0600

    Added safey checks for PushEvent commits

--- cli/github-user-activity/utils/formatEvents.js
+++ cli/github-user-activity/utils/formatEvents.js
@@ -2,8 +2,8 @@ function formatEvents(events) {
     return events.map(event => {
 
         if (event.type === "PushEvent") {
-            const commitCount = event.payload.commits.length;
-            const repoName = event.repo.name;
+            const commitCount = event.payload?.commits?.length || 0;
+            const repoName = event.repo?.name || "unknown repository";
 
             return `Pushed ${commitCount} commit(s) to ${repoName}`;
         }
