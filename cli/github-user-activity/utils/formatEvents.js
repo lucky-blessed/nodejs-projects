65e3a280cee150302ffb51849d6beefe518cdd39
lucky-blessed <blessednkwor7@gmail.com>
2026-03-09 01:34:07 -0600

    Hot fix

--- cli/github-user-activity/utils/formatEvents.js
+++ cli/github-user-activity/utils/formatEvents.js
@@ -1,13 +1,13 @@
 function formatEvents(events) {
     return events.map(event => {
 
-        if (event.type === "PushedEvent") {
+        if (event.type === "PushEvent") {
             const commitCount = event.payload.commits.length;
             const repoName = event.repo.name;
 
             return `Pushed ${commitCount} commit(s) to ${repoName}`;
         }
-        
+
         return event.type;
     });
 }
